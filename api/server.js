750d5c32936057516444bcda81779b72ab378bae
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-06-22 21:03:05 -0700

    added authentication files

--- api/server.js
+++ api/server.js
@@ -2,7 +2,9 @@ const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
 
-const usersRouter = requre('../users/users-router.js')
+const usersRouter = require('../users/users-router.js')
+const postsRouter = require('../posts/posts-router.js')
+
 const server = express()
 
 server.use(helmet());
@@ -10,5 +12,6 @@ server.use(cors());
 server.use(express.json());
 
 server.use('/api/users', usersRouter)
+server.use('/api/posts', postsRouter)
 
 module.exports = server
