258d5d0addab647318e4c7ad7e10bb81e1d14555
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-06-22 21:19:44 -0700

    minor file changes

--- api/server.js
+++ api/server.js
@@ -14,4 +14,8 @@ server.use(express.json());
 server.use('/api/users', usersRouter)
 server.use('/api/posts', postsRouter)
 
+server.get("/", (req, res) => {
+    res.json({ api: "up"})
+})
+
 module.exports = server
