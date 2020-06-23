41911f5fb9777a1db05f9dcd5922722ef2b75d67
Justin Lohner <lohner4@gmail.com>
2020-06-23 12:44:20 -0600

    Wrote basic user, role, and userrole classes

---
+++ subredditpredictor/src/main/java/com/lambdaschool/subredditpredictor/models/Role.java
@@ -0,0 +1,57 @@
+package com.lambdaschool.subredditpredictor.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "roles")
+public class Role extends Audit {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long roleId;
+
+	@Column(nullable = false, unique = true)
+	private String name;
+
+	@OneToMany(mappedBy = "role", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties(value = "role", allowSetters = true)
+	private List<UserRole> users = new ArrayList<>();
+
+	public Role() {
+	}
+
+	public Role(String name) {
+		this.name = name.toUpperCase();
+	}
+
+	public long getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(long roleId) {
+		this.roleId = roleId;
+	}
+
+	public String getName() {
+		if (name == null) {
+			return null;
+		} else {
+			return name.toUpperCase();
+		}
+	}
+
+	public void setName(String name) {
+		this.name = name.toUpperCase();
+	}
+
+	public List<UserRole> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<UserRole> users) {
+		this.users = users;
+	}
+}
---
+++ subredditpredictor/src/main/java/com/lambdaschool/subredditpredictor/models/User.java
@@ -0,0 +1,85 @@
+package com.lambdaschool.subredditpredictor.models;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User extends Audit {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long userId;
+
+	@Column(nullable = false, unique = true)
+	private String username;
+
+	@Column(nullable = false)
+	private String password;
+
+	// @Column(nullable = false, unique = true)
+	// @Email
+	// private String primaryEmail;
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+	private List<UserRole> roles = new ArrayList<>();
+
+	public User() {
+	}
+
+	public User(String username, String password, List<UserRole> userRoles) {
+		setUsername(username);
+		setPassword(password);
+		for (UserRole ur : userRoles) {
+			ur.setUser(this);
+		}
+		this.roles = userRoles;
+	}
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		if (username == null) {
+			return null;
+		} else {
+			return username.toLowerCase();
+		}
+	}
+
+	public void setUsername(String username) {
+		this.username = username.toLowerCase();
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPasswordNoEncrypt(String password) {
+		this.password = password;
+	}
+
+	public void setPassword(String password) {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		this.password = passwordEncoder.encode(password);
+	}
+
+	public List<UserRole> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<UserRole> roles) {
+		this.roles = roles;
+	}
+
+	public void addRole(Role role) {
+		roles.add(new UserRole(this, role));
+	}
+}
