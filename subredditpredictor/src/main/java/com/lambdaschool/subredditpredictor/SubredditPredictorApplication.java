65ac51cef7de61c5ce449e70519e0c936ac37afb
Justin Lohner <lohner4@gmail.com>
2020-06-22 19:06:50 -0600

    Created spring boot project

---
+++ subredditpredictor/src/main/java/com/lambdaschool/subredditpredictor/SubredditPredictorApplication.java
@@ -0,0 +1,13 @@
+package com.lambdaschool.subredditpredictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubredditPredictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubredditPredictorApplication.class, args);
+	}
+
+}
