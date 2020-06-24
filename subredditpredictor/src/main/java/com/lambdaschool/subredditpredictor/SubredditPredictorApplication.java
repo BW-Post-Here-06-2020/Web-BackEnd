af232e1538a0771115b3c7e3e1a8ee3ea55f17ab
Justin Lohner <lohner4@gmail.com>
2020-06-24 15:08:13 -0600

    Added environment variables where needed

--- subredditpredictor/src/main/java/com/lambdaschool/subredditpredictor/SubredditPredictorApplication.java
+++ subredditpredictor/src/main/java/com/lambdaschool/subredditpredictor/SubredditPredictorApplication.java
@@ -1,13 +1,37 @@
 package com.lambdaschool.subredditpredictor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SubredditPredictorApplication {
+	// for local without env variables
+	// public static void main(String[] args) {
+	// 	SpringApplication.run(SubredditPredictorApplication.class, args);
+	// }
+
+	@Autowired
+	private static Environment env;
+
+	private static boolean stop = false;
+
+	private static void checkEnvironmentVariable(String envVar) {
+		if (System.getenv(envVar) == null) {
+			stop = true;
+		}
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(SubredditPredictorApplication.class, args);
+		checkEnvironmentVariable("OAUTHCLIENTID");
+		checkEnvironmentVariable("OAUTHCLIENTSECRET");
+
+		if (!stop) {
+			SpringApplication.run(SubredditPredictorApplication.class, args);
+		}
 	}
 
 }
